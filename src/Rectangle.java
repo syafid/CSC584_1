02ea5f5b759ec1fbbaa07b94bf5e6c6af3f486b8
syafid <syafid@ppj.gov.my>
2022-11-26 11:09:03 +0800

    commit on 26nov22
    commit on 26nov22

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,13 +1,14 @@
 public class Rectangle {
-	int width;
-	int height;
+	public static int width;
+	public static int height;
 	
 	int area() {
 		return width * height;
 	}
 	public static void main(String[] args) {
 		Rectangle myReact = new Rectangle();
-	    myRect.width = 40;
+	    
+		myRect.width = 40;
 	    myRect.height = 50;
 	    System.out.println("myRect's area is " +          myRect.area());
 
